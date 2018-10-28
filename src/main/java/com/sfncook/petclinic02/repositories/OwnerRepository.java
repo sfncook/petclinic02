32af1b33779fe45f189cd0dc4e8616129f71b4a7
Shawn Cook <cooksha@yahoo-inc.com>
2018-10-28 08:56:20 -0700

    findByPets

--- src/main/java/com/sfncook/petclinic02/repositories/OwnerRepository.java
+++ src/main/java/com/sfncook/petclinic02/repositories/OwnerRepository.java
@@ -1,10 +1,11 @@
 package com.sfncook.petclinic02.repositories;
 
 import com.sfncook.petclinic02.models.Owner;
+import com.sfncook.petclinic02.models.Pet;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-    List<Owner> findByPetsIn(Long petId);
+    List<Owner> findByPets(Pet pet);
 }
