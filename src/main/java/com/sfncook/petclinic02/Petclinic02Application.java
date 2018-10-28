3fa93c688e3f5bf0fc84c2a2ec308506c6255975
Shawn Cook <cooksha@yahoo-inc.com>
2018-10-27 22:07:18 -0700

    init

---
+++ src/main/java/com/sfncook/petclinic02/Petclinic02Application.java
@@ -0,0 +1,12 @@
+package com.sfncook.petclinic02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petclinic02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Petclinic02Application.class, args);
+	}
+}
